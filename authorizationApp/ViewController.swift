b8708bdd7060a36cb448c5b01f467b91e020b3cf
ArtemMana <47473922+ArtemMana@users.noreply.github.com>
2020-10-15 13:56:56 +0300

    Add files via upload

---
+++ authorizationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  authorizationApp
+//
+//  Created by Artem Manakov on 14.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
